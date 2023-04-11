aea5016ffcb0bbf935ec29d72352e8f9d61bf42b
mandarinrock <mandarinrock.dg@gmail.com>
2023-04-11 13:52:42 -0700

    imported guess lists into main

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import word_lists
+
 def main():
     print("Hello World!")
+    allowed_guesses = word_lists.allowed_guesses
+    print(allowed_guesses[0])
 
 if __name__ == "__main__":
     main()
