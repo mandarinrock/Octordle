cc384f0ac9bd1ebc042f819a2a6fe6672e8e19f4
mandarinrock <mandarinrock.dg@gmail.com>
2023-04-13 19:21:14 -0700

    Applied new fitness function to generation script

--- genetic.py
+++ genetic.py
@@ -1,12 +1,15 @@
 import word_lists
+from itertools import combinations
+
 permitted_answers = word_lists.allowed_answers
 permitted_guesses = word_lists.allowed_guesses
 
-import random
-from itertools import permutations, product, combinations
-
 def calculate_match_score(word1, word2):
-    return sum(1 for a, b in zip(word1, word2) if a == b)
+    return len(set(word1) & set(word2))
+
+def sequence_penalty(sequence):
+    unique_letter_coverage = len(set(''.join(sequence)))
+    return -0.1 * unique_letter_coverage
 
 def find_best_starting_sequence(permitted_guesses, permitted_answers, seq_length=3):
     best_fitness = 0
@@ -19,8 +22,11 @@ def find_best_starting_sequence(permitted_guesses, permitted_answers, seq_length
             for answer in permitted_answers:
                 total_match_score += calculate_match_score(guess, answer)
         
-        if total_match_score > best_fitness:
-            best_fitness = total_match_score
+        penalty = sequence_penalty(sequence)
+        fitness = total_match_score - penalty
+
+        if fitness > best_fitness:
+            best_fitness = fitness
             best_sequence = sequence
 
         if iteration % 100 == 0:
